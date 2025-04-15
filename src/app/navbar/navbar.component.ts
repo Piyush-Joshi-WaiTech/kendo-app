243aa0bb4da22f02c74a2bdc7afffcf5bce1570e
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-04-15 18:50:01 +0530

    feat: Implement dark mode styles and toggle functionality for improved user experience

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -21,4 +21,17 @@ export class NavbarComponent {
     { label: 'WORKFLOWS', icon: 'bi-recycle' },
     { label: 'CHAT', icon: 'bi-chat-dots', badge: 1 },
   ];
+
+  isNightMode: boolean = false;
+
+  toggleNightMode(): void {
+    this.isNightMode = !this.isNightMode;
+    const body = document.body;
+
+    if (this.isNightMode) {
+      body.classList.add('dark-mode');
+    } else {
+      body.classList.remove('dark-mode');
+    }
+  }
 }
